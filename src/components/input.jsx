536d27eb261303bcbf0f9e0c6a670dd8a59eccc9
Conor Devine <conorjdevine@gmail.com>
2017-04-25 13:40:13 -0400

    move errors to hoc

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,49 +1,17 @@
 import React from 'react';
+import withErrors from './withErrors';
 import Error from './error';
-import { setClassName } from '../helpers'
+import { setClassName } from '../helpers';
 
-export default class Input extends React.Component {
+class Input extends React.Component {
   constructor() {
     super();
-    this.state = {
-      errorMessage: false,
-      displayErrors: false
-    }
     this._onChange = this._onChange.bind(this);
-    this._hideErrors = this._hideErrors.bind(this);
-    this._displayErrors = this._displayErrors.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.validate({[this.props.id]: !this.props.required});
-    this._handleErrors(this.props.value);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    !this.props.displayErrors && nextProps.displayErrors && this.setState({ displayErrors: true })
-  }
-
-  _handleErrors(value) {
-    let errorMessage = false;
-    if (this.props.required && !value) {
-      errorMessage = "You can't leave this empty";
-    } else if (value) {
-      for (var i = 0; i < this.props.errors.length; i++ ) {
-        var error = this.props.errors[i];
-        if (error._handle(value)) {
-          errorMessage = error.message;
-        }
-      }
-    }
-
-
-    this.setState({ errorMessage });
-    this.props.validate({[this.props.id]: !errorMessage})
   }
 
   _onChange(e) {
     const value = e.currentTarget.value;
-    this._handleErrors(value);
+    this.props.handleErrors(value);
     this.props.onChange(value);
   }
 
@@ -61,22 +29,22 @@ export default class Input extends React.Component {
             id, type, inputClass,
             underline,
             validate, required, errors, onChange,
+            errorMessage, displayErrors, _displayErrors, _hideErrors, handleErrors,
             ...props
           } = this.props;
-    const { displayErrors, errorMessage } = this.state;
     return (
       <div className={setClassName('input-container', containerClass)}>
         <label htmlFor={id} className={labelClass}>{labelText}</label>
         <input
           id={id}
           type={type}
-          className={inputClass + ((errorMessage && displayErrors) ? ' error' : '')}
+          className={setClassName(inputClass, (errorMessage && displayErrors && 'error'))}
           onChange={ this._onChange }
-          onFocus={ this._hideErrors }
-          onBlur={ this._displayErrors }
+          onFocus={ _hideErrors }
+          onBlur={ _displayErrors }
           {...props}
           />
-        <Error { ...this.state } />
+        <Error errorMessage={errorMessage} displayErrors={displayErrors} />
         {underline && <p>{underline}</p>}
       </div>
     );
@@ -98,4 +66,4 @@ Input.defaultProps = {
   validate: () => {}
 }
 
-// length undefined means slice equals string it's called on
+export default withErrors(Input);
---
+++ src/components/withErrors.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const withErrors = Input => class extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      errorMessage: '',
+      displayErrors: false
+    }
+    this._hideErrors = this._hideErrors.bind(this);
+    this._displayErrors = this._displayErrors.bind(this);
+    this._handleErrors = this._handleErrors.bind(this);
+  }
+
+  componentDidMount() {
+    // this.props.validate({[this.props.id]: !this.props.required});
+    this._handleErrors(this.props.value);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    !this.props.displayErrors && nextProps.displayErrors && this.setState({ displayErrors: true })
+  }
+
+  _handleErrors(value) {
+    let errorMessage = false;
+    if (this.props.required && !value) {
+      errorMessage = "You can't leave this empty";
+    } else if (value && this.props.errors) {
+      for (var i = 0; i < this.props.errors.length; i++ ) {
+        var error = this.props.errors[i];
+        if (error._handle(value)) {
+          errorMessage = error.message;
+        }
+      }
+    }
+
+
+    this.setState({ errorMessage });
+    // this.props.validate({[this.props.id]: !errorMessage})
+  }
+
+  _displayErrors() {
+    this.setState({ displayErrors: true });
+  }
+
+  _hideErrors() {
+    this.setState({ displayErrors: false });
+  }
+
+  render() {
+    return <Input handleErrors={this._handleErrors} _displayErrors={this._displayErrors} _hideErrors={this._hideErrors} {...this.props} {...this.state} />;
+  }
+}
+
+export default withErrors;
