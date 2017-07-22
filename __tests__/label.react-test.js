c76846fa85cf2e7f540eb7c91af005e057e98ff9
Conor Devine <conorjdevine@gmail.com>
2017-07-22 16:48:11 -0400

    add separate tests for new label component

---
+++ __tests__/label.react-test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Label from '../src/components/label';
+import renderer from 'react-test-renderer';
+
+describe("Label", () => {
+  test("does not render without child", () => {
+    const tree = renderer.create(
+      <Label
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("renders with child", () => {
+    const tree = renderer.create(
+      <Label>Test Label</Label>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("accepts props", () => {
+    const tree = renderer.create(
+      <Label
+        label="Test Label"
+        htmlFor="htmlFor"
+        id="labelId"
+        className="label-class test"
+        >
+        Test Input
+      </Label>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
