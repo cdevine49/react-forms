411179a185aeb73bfd54fcdeb45ef26a3967586f
Conor Devine <conorjdevine@gmail.com>
2017-07-22 15:45:46 -0400

    test confirmation component in isolation

--- __tests__/confirmation.react-test.js
+++ __tests__/confirmation.react-test.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import Confirmation from '../src/components/confirmation';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
-describe("Match", () => {
-  let wrapper, input;
+describe("Confirmation", () => {
   const match = 'test';
   const miss = "abcd"
   const message = "Should equal 'test'"
-  beforeEach(() => {
-    wrapper = mount(
-      <Confirmation
-        match={match}
-        message={message}
-        />
-    );
-    input = wrapper.find('input');
+
+  const container = shallow(
+    <Confirmation
+      match={match}
+      message={message}
+      />
+  );
+  const errors = container.props().errors;
+  const error = errors[0];
+
+  test("Errors prop has length of one", () => {
+    expect(errors.length).toBe(1);
   });
 
-  test("If confirmation value equals match value, no error message appears", () => {
-    wrapper.setProps({ value: match });
-    input.simulate('blur');
-    expect(wrapper.find('span').exists()).toBe(false);
+  test("Error prop's handle function returns false when value is equivalent to match", () => {
+    expect(error._handle(match)).toBe(false);
   });
 
-  test("If confirmation value does not equal match value, error message appears", () => {
-    wrapper.setProps({ value: miss });
-    input.simulate('blur');
-    expect(wrapper.find('span').text()).toBe(message);
+  test("Error prop's handle function returns true when value is not equivalent to match", () => {
+    expect(error._handle(miss)).toBe(true);
+    expect(error._handle(undefined)).toBe(true);
+    expect(error._handle(null)).toBe(true);
+    expect(error._handle(false)).toBe(true);
+  });
+
+  test("Error prop's message equals Confirmation's message prop", () => {
+    expect(error.message).toBe(message)
   });
 });
