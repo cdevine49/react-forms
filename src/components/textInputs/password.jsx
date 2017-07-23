bf491db1ab858a753697d70a96531bbe1c530caa
Conor Devine <conorjdevine@gmail.com>
2017-07-23 16:15:19 -0400

    updates to password component

--- src/components/textInputs/password.jsx
+++ src/components/textInputs/password.jsx
@@ -8,14 +8,23 @@ const Password = (props) => {
 }
 
 Password.defaultProps = {
-  labelText: 'Password',
+  label: 'Password',
   type: 'password',
   errors: [
     {
-      _handle: (v) => v.length < 8, message: "Password must have at least 8 characters",
+      _handle: (v) => v.length < 8, message: "Password must have at least 8 characters"
     },
     {
-      _handle: (v) => v.length > 100, message: "Password must be shorter than 100 characters",
+      _handle: (v) => v.length > 100, message: "Password must be shorter than 100 characters"
+    },
+    {
+      _handle: (v) => !/\d/.test(v), message: "Password must include at least one number"
+    },
+    {
+      _handle: (v) => !/[a-z]/.test(v), message: "Password must include at least one lowercase letter"
+    },
+    {
+      _handle: (v) => !/[A-Z]/.test(v), message: "Password must include at least one uppercase letter"
     }
   ]
 }
