36f6804bf7f928e05292a67a1a343ff31135c1d4
Conor Devine <conorjdevine@gmail.com>
2017-07-23 16:21:14 -0400

    write tests for submit component

---
+++ __tests__/submit.react-test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Submit from '../src/components/submit';
+import renderer from 'react-test-renderer';
+
+describe("Submit", () => {
+  test('renders with default props', () => {
+    const tree = renderer.create(
+      <Submit
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('accepts new props', () => {
+    const tree = renderer.create(
+      <Submit
+        id="submitId"
+        className="classy submit"
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
