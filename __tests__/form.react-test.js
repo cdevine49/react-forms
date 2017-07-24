787606ee4bb555aade0b075e666e156b5b385356
Conor Devine <conorjdevine@gmail.com>
2017-07-24 17:21:13 -0400

    start testing form

---
+++ __tests__/form.react-test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Form from '../src/components/form';
+import TextInput from '../src/components/textInput';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+describe("Form", () => {
+  describe("Default", () => {
+    test("renders with default props", () => {
+      const tree = renderer.create(
+        <Form
+          />
+      ).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  test("accepts props", () => {
+    const tree = renderer.create(
+      <Form
+        id="formId"
+        className="new-class"
+        noValidate={false}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe("Submission", () => {
+    let onSubmit;
+    const form = mount(
+      <Form>
+        <input type="submit" />
+      </Form>
+    );
+
+    beforeEach(() => {
+      onSubmit = jest.fn();
+      form.setProps({ onSubmit });
+    });
+
+    test("calls onSubmit prop when submittable array empty", () => {
+
+
+      form.setState({ submittable: [] });
+      expect(onSubmit.mock.calls.length).toBe(0);
+      form.simulate('submit');
+      expect(onSubmit.mock.calls.length).toBe(1);
+    });
+
+    test("calls onSubmit prop when submittable array elements all truthy", () => {
+      form.setState({ submittable: [ true, 'true', 1] });
+      expect(onSubmit.mock.calls.length).toBe(0);
+      form.simulate('submit');
+      expect(onSubmit.mock.calls.length).toBe(1);
+    });
+
+    test("does not call onSubmit prop when any submittable array elements falsey", () => {
+      expect(onSubmit.mock.calls.length).toBe(0);
+      form.setState({ submittable: [ true, 'true', false, 1] });
+      form.simulate('submit');
+
+      expect(onSubmit.mock.calls.length).toBe(0);
+
+      form.setState({ submittable: [ true, 'true', '', 1] });
+      form.simulate('submit');
+      expect(onSubmit.mock.calls.length).toBe(0);
+    });
+  });
+});
