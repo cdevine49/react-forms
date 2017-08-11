d9e8eee0ce1dd1d47178891c273adeb9d196b0e0
Conor Devine <conorjdevine@gmail.com>
2017-08-11 17:28:27 -0400

    use setclassname

--- src/components/textFields/flagFields/shared/flag.jsx
+++ src/components/textFields/flagFields/shared/flag.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import setClassName from '../../../../helpers/setClassName';
 
 const Flag = ({className, offset, name, code}) => {
-  var className = 'flag ' + className;
+  var className = setClassName(['flag', className]);
   return (
     <div className={className} style={{'backgroundImage': "url(/images/flags.png)", 'backgroundPositionX': '0px', 'backgroundPositionY': offset}}></div>
   );
