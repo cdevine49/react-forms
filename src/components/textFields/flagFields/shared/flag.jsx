38abdc3497f56a28519367826aa89dd27dbd26ed
Conor Devine <conorjdevine@gmail.com>
2017-08-12 19:50:26 -0400

    wasn't using code either

--- src/components/textFields/flagFields/shared/flag.jsx
+++ src/components/textFields/flagFields/shared/flag.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import setClassName from '../../../../helpers/setClassName';
 
-const Flag = ({ className, code, offset }) => {
+const Flag = ({ className, offset }) => {
   var className = setClassName(['flag', className]);
   return (
     <div className={className} style={{'backgroundImage': "url(/images/flags.png)", 'backgroundPositionX': '0px', 'backgroundPositionY': offset}}></div>
