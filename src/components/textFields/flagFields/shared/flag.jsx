ad7d6c4d0c5028c6fd8b0797db8b1bc31b5f263b
Conor Devine <conorjdevine@gmail.com>
2017-08-12 19:47:55 -0400

    remove name prop

--- src/components/textFields/flagFields/shared/flag.jsx
+++ src/components/textFields/flagFields/shared/flag.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import setClassName from '../../../../helpers/setClassName';
 
-const Flag = ({className, offset, name, code}) => {
+const Flag = ({ className, code, offset }) => {
   var className = setClassName(['flag', className]);
   return (
     <div className={className} style={{'backgroundImage': "url(/images/flags.png)", 'backgroundPositionX': '0px', 'backgroundPositionY': offset}}></div>
