84dc244bc07a6329e1f46aa2178b420771b0422a
Conor Devine <conorjdevine@gmail.com>
2017-08-22 21:45:34 -0400

    handle selecting country under flagbox in part for accessibility

--- src/components/textFields/flagFields/flagInput/flagbox.jsx
+++ src/components/textFields/flagFields/flagInput/flagbox.jsx
@@ -1,15 +1,59 @@
 import React from 'react';
 import Flag from '../shared/flag';
 import FlagArray from '../shared/countries';
+import Countries from '../countries';
 
-const Flagbox = ({onClick, countryIndex}) => {
-  const [offset, name, code] = FlagArray[countryIndex];
-  return (
-    <button type="button" className={'flagbox'} onClick={onClick}>
-      <Flag offset={offset} />
-      <img src="/images/select-box-button.png" className='select-box-button' />
-    </button>
-  );
+export default class FlagBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectingCountry: false
+    }
+
+    this.toggleSelecting = this.toggleSelecting.bind(this);
+    this.selectCountry = this.selectCountry.bind(this);
+  }
+
+  toggleSelecting() {
+    this.setState((prevState, props) => ({ selectingCountry: !prevState.selectingCountry }));
+  }
+
+  selectCountry() {
+    return i => {
+      this.setState({ selectingCountry: false }, () => {
+        this.flagButton.focus();
+        this.props.onChange(i);
+      });
+    }
+  }
+
+  render() {
+    const { selectingCountry } = this.state;
+    const { countryInfoIndex, countryIndex } = this.props;
+    const [offset] = FlagArray[countryIndex];
+
+    return (
+      <div className="flagbox-container">
+        <button
+          type="button"
+          aria-pressed={selectingCountry}
+          className="flagbox"
+          onClick={this.toggleSelecting}
+          ref={ button => { this.flagButton = button; } }>
+          <Flag offset={offset} />
+          <img src="/images/select-box-button.png" className='select-box-button' />
+        </button>
+        {selectingCountry && <Countries
+            countryInfoIndex={countryInfoIndex}
+            countryIndex={countryIndex}
+            onClick={this.selectCountry()} />}
+      </div>
+    );
+  }
 };
 
-export default Flagbox;
+FlagBox.defaultProps = {
+  countryInfoIndex: 2,
+  countryIndex: 0,
+  onChange: function(){}
+}
