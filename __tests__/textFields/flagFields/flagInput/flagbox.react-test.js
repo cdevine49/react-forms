334c9a2f4e7116578f537a03b6dd8cda1b62107c
Conor Devine <conorjdevine@gmail.com>
2017-08-22 21:57:46 -0400

    write first flagbox snapshot

--- __tests__/textFields/flagFields/flagInput/flagbox.react-test.js
+++ __tests__/textFields/flagFields/flagInput/flagbox.react-test.js
@@ -1,3 +1,13 @@
 import React from 'react';
-import countries from '../../../../src/components/textFields/flagFields/shared/countries';
+import Flagbox from '../../../../src/components/textFields/flagFields/flagInput/flagbox';
 import renderer from 'react-test-renderer';
+
+describe("Flagbox", () => {
+  test("Without Props", () => {
+    const tree = renderer.create(
+      <Flagbox />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
