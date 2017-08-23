e410ae9665fe43cb19a8ec54dc01bd42ff1876d8
Conor Devine <conorjdevine@gmail.com>
2017-08-22 22:25:59 -0400

    test everything but selecting a country

--- __tests__/textFields/flagFields/flagInput/flagbox.react-test.js
+++ __tests__/textFields/flagFields/flagInput/flagbox.react-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Flagbox from '../../../../src/components/textFields/flagFields/flagInput/flagbox';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe("Flagbox", () => {
   test("Without Props", () => {
@@ -23,5 +24,44 @@ describe("Flagbox", () => {
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
-  })
+  });
+
+  test("Flag button toggles selectingCountry", () => {
+    const component = shallow(
+      <Flagbox />
+    );
+    const button = component.find('button');
+    const click = () => button.simulate('click');
+
+    expect((component).state('selectingCountry')).toBe(false);
+    click();
+    expect((component).state('selectingCountry')).toBe(true);
+    const j = Math.round(Math.random() * 10);
+    for (let i = 0; i < j; i++) { click() }
+    expect((component).state('selectingCountry')).toBe(j % 2 === 0);
+  });
+
+  describe("Countries", () => {
+    test('Only renders when selecting country', () => {
+      const component = shallow(
+        <Flagbox />
+      );
+
+      expect(component.children().length).toBe(1);
+      component.setState({ selectingCountry: true });
+      expect(component.children().length).toBe(2);
+      component.setState({ selectingCountry: false });
+      expect(component.children().length).toBe(1);
+    });
+
+    describe("Selecting country", () => {
+      test("Sets selectingCountry to false", () => {
+
+      });
+
+      test("Calls onChange prop with the country index", () => {
+
+      });
+    })
+  });
 });
