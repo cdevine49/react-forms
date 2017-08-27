d6f9b04ef0d1f79de224a259f86bb83d505fa42d
Conor Devine <conorjdevine@gmail.com>
2017-08-27 17:22:00 -0400

    test text input events

--- __tests__/textFields/flagFields/flagField.react-test.js
+++ __tests__/textFields/flagFields/flagField.react-test.js
@@ -13,20 +13,6 @@ describe("FlagField", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('props populate the input tag', () => {
-    const tree = renderer.create(
-      <FlagField
-        id="test-input"
-        className="test-class another class"
-        value="Can set a default value"
-        type="email"
-        onChange={jest.fn()}
-        />
-    ).toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-
   test('FieldProps', () => {
     const tree = renderer.create(
       <FlagField
@@ -78,33 +64,89 @@ describe("FlagField", () => {
     });
   });
 
-  test('inputContainerProps', () => {
-    const tree = renderer.create(
-      <FlagField
-        inputContainerProps={{
-          id: "test-input",
-          className: "test-class another class",
-          onMouseDown: jest.fn()
-        }}
-        />
-    ).toJSON();
+  describe('FlagInput', () => {
+    test('inputContainerProps', () => {
+      const tree = renderer.create(
+        <FlagField
+          inputContainerProps={{
+            id: "test-input",
+            className: "test-class another class",
+            onMouseDown: jest.fn()
+          }}
+          />
+      ).toJSON();
 
-    expect(tree).toMatchSnapshot();
+      expect(tree).toMatchSnapshot();
+    });
+
+    test('props populate the input tag', () => {
+      const tree = renderer.create(
+        <FlagField
+          id="test-input"
+          className="test-class another class"
+          value="Can set a default value"
+          type="email"
+          onChange={jest.fn()}
+          />
+      ).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+
+    describe("Text Input", () => {
+      const flagfield = shallow(
+        <FlagField />
+      );
+
+      const onBlur = jest.fn();
+      const onFocus = jest.fn();
+      const onChange = jest.fn();
+      const root = flagfield.first().shallow();
+      root.setProps({ onBlur, onFocus, onChange });
+      const input = root.find('input');
+
+      test("blur event calls onBlur prop", () => {
+        expect(onBlur).not.toHaveBeenCalled();
+        input.simulate('blur');
+        expect(onBlur).toHaveBeenCalled();
+      });
+
+      test("focus event calls onFocus prop", () => {
+        expect(onFocus).not.toHaveBeenCalled();
+        input.simulate('focus');
+        expect(onFocus).toHaveBeenCalled();
+      });
+
+      test('onChange event calls onChange prop with text input value', () => {
+        const value = "First Last";
+        const e = { target: { value } }
+
+        expect(onChange).not.toHaveBeenCalled();
+        input.simulate('change', e);
+        expect(onChange).toHaveBeenCalledWith(e);
+      });
+    });
   });
 
-  // test('ErrorProps', () => {
-  //   const tree = renderer.create(
-  //     <FlagField
-  //       errorMessage="I exist too"
-  //       errorProps={{
-  //         id: "whoopsie",
-  //         className: "way to go you jerk"
-  //       }}
-  //       />
-  //   ).toJSON();
-  //
-  //   expect(tree).toMatchSnapshot();
-  // });
+
+  describe('Errors', () => {
+    const flagfield = shallow(
+      <FlagField />
+    );
+
+    const root = flagfield.first().shallow();
+    const errorMessage = "Whoopsie"
+    root.setProps({ errorMessage });
+
+    test('receives errorMessage prop as child', () => {
+      const error = root.childAt(2);
+      expect(error.props().children).toBe(errorMessage);
+    });
+  });
+
+  describe('Underline', () => {
+
+  });
   //
   // test('UnderlineProps', () => {
   //   const tree = renderer.create(
