a848ae72d0460d71c2c1b8eee69b26987fc3a661
Conor Devine <conorjdevine@gmail.com>
2017-09-10 15:32:32 -0400

    set up radio button test file

---
+++ __tests__/radio/button.react-test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RadioGroup from '../../src/components/radio/group';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+describe('RadioButton', () => {
+
+});
