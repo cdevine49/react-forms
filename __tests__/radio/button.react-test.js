af15d81855ba1712e925260cc2bfa35853244978
Conor Devine <conorjdevine@gmail.com>
2017-09-10 15:47:05 -0400

    clicking container calls onchange function

--- __tests__/radio/button.react-test.js
+++ __tests__/radio/button.react-test.js
@@ -4,5 +4,29 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 describe('RadioButton', () => {
+  test('Default', () => {
+    const tree = renderer.create(
+      <RadioButton
+        />
+    ).toJSON();
 
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe('Warnings', () => {
+
+  });
+
+  describe('Container', () => {
+    test('OnClick calls the onChange prop', () => {
+      const onChange = jest.fn();
+      const button = shallow(
+        <RadioButton onChange={ onChange } />
+      );
+
+      expect(onChange).not.toHaveBeenCalled();
+      button.simulate('click');
+      expect(onChange).toHaveBeenCalled();
+    });
+  });
 });
