c773256bcc2f5418f1265aaf2a1255c47ce68d2f
Conor Devine <conorjdevine@gmail.com>
2017-09-10 15:29:52 -0400

    add onchange and value to input and warn when missing expected values

--- src/components/radio/button.jsx
+++ src/components/radio/button.jsx
@@ -8,33 +8,39 @@ const RadioButton = (
     containerProps: {
       containerClass,
       ...containerProps
-    }
+    },
     id,
     label,
     labelProps: {
       className: labelClass,
       ...labelProps
     },
+    onChange,
     value,
     ...props
   }
 ) => {
+  !id && console.warn("RadioButton expects an id");
+  !value && console.warn("RadioButton expects a value");
 
   return (
-    <div className={ setClassName(['radio-button', containerClass]) } { ...containerProps }>
+    <div className={ setClassName(['radio-button', containerClass]) } onClick={ onChange } { ...containerProps }>
       <input
         id={ id }
         type='radio'
         className={ setClassName(['radio-input', className]) }
+        onChange={ onChange }
+        value={ value }
         { ...props }
         />
-      <Label htmlFor={ id } className={ setClassName(['radio-label', labelClass]) }>{ label }</Label>
+      <Label htmlFor={ id } className={ setClassName(['radio-label', labelClass]) } { ...labelProps } >{ label || value }</Label>
     </div>
   );
 }
 RadioButton.defaultProps = {
+  containerProps: {},
   id: '',
-  labelText: '',
+  labelProps: {},
   value: null
 }
 
