27ad7ec6ce567a1d8d26993b669aaf19a2cd33c9
Conor Devine <conorjdevine@gmail.com>
2017-10-09 23:20:32 -0400

    test props directly

--- __tests__/textInput.react-test.js
+++ __tests__/textInput.react-test.js
@@ -11,225 +11,154 @@ describe('TextInput', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  describe('ContainerProps', () => {
-    test('populate the container tag', () => {
-      const tree = renderer.create(
-        <TextInput
-          containerProps={{
-            id: 'container',
-            style: { margin: "10px" }
-          }}
-          />
-      ).toJSON();
+  test('containerProps', () => {
+    const tree = renderer.create(
+      <TextInput
+        containerProps={{
+          id: 'container',
+          className: 'classy react',
+          style: { margin: "10px" }
+        }}
+        />
+    ).toJSON();
 
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('className added to container tag\'s default classes', () => {
-      const tree = renderer.create(
-        <TextInput
-          containerProps={{
-            className: 'classy react',
-          }}
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
   });
 
-  describe('Label', () => {
-    test('label prop acts as child', () => {
-      const tree = renderer.create(
-        <TextInput
-          label="Text Input"
-          />
-      ).toJSON();
+  test('label', () => {
+    const tree = renderer.create(
+      <TextInput
+        label="Child/Text"
+        />
+    ).toJSON();
 
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('htmlFor populated by id prop', () => {
-      const tree = renderer.create(
-        <TextInput
-          id="first-name"
-          label="First Name"
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('labelProps populate the label tag', () => {
-      const tree = renderer.create(
-        <TextInput
-          label="Last Name"
-          labelProps={{
-            id: 'my-label',
-            className: 'last name label',
-            style: { fontSize: "16px" }
-          }}
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
   });
 
-  describe('Input', () => {
-    test('receives props by default', () => {
-      const tree = renderer.create(
-        <TextInput
-          id="my-input"
-          className="big and fat"
-          style={{ padding: "25px 15px" }}
-          data-key="input-key"
-          />
-      ).toJSON();
+  test('id', () => {
+    const tree = renderer.create(
+      <TextInput
+        id="first-name"
+        label="First Name"
+        />
+    ).toJSON();
 
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('receives \'error\' class and aria-invalid when errorMessage prop is truthy', () => {
-      let tree = renderer.create(
-        <TextInput
-          errorMessage={true}
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-
-      tree = renderer.create(
-        <TextInput
-          className="big and fat"
-          errorMessage="Ya dun goofed"
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-
-      tree = renderer.create(
-        <TextInput
-          errorMessage=""
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('receives aria-required when required', () => {
-      const tree = renderer.create(
-        <TextInput
-          required={true}
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-
-    describe('aria-describedby', () => {
-      test('receives underlineProps.id', () => {
-        const tree = renderer.create(
-          <TextInput
-            underlineProps={{
-              id: "descriptor"
-            }}
-            ariaDescribedby="secondary"
-            />
-        ).toJSON();
-
-        expect(tree).toMatchSnapshot();
-      });
-
-      test('receives errorProps.id', () => {
-        const tree = renderer.create(
-          <TextInput
-            errorProps={{ id: "error-description" }}
-            />
-        ).toJSON();
-
-        expect(tree).toMatchSnapshot();
-      })
-    });
+    expect(tree).toMatchSnapshot();
   });
 
-  describe('Error', () => {
-    test('errorMessage acts as child prop', () => {
+  test('labelProps', () => {
+    const tree = renderer.create(
+      <TextInput
+        label="Last Name"
+        labelProps={{
+          id: 'my-label',
+          className: 'right large name',
+          style: { fontSize: "16px" }
+        }}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('className', () => {
+    const tree = renderer.create(
+      <TextInput
+        className="big and fat"
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('required', () => {
+    const tree = renderer.create(
+      <TextInput
+        label="Gets an Asterisk"
+        required={true}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('errorMessage', () => {
+    const tree = renderer.create(
+      <TextInput
+        className="not overridden"
+        errorMessage="Ya dun goofed"
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('errorProps', () => {
+    const tree = renderer.create(
+      <TextInput
+        errorMessage="Uh oh"
+        errorProps={{
+          id: 'error-tag',
+          className: 'more classes',
+          'data-message': 'hi there'
+        }}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('underline', () => {
+    const tree = renderer.create(
+      <TextInput
+        underline="More explanation"
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('underlineProps', () => {
+    const tree = renderer.create(
+      <TextInput
+        underline="Fill this in because ..."
+        underlineProps={{
+          id: "underline-tag",
+          className: 'help explain',
+          style: { background: 'blue' }
+        }}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('other props', () => {
+    const tree = renderer.create(
+      <TextInput
+        onFocus={jest.fn()}
+        style={{ padding: "25px 15px" }}
+        data-key="input-key"
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe('aria-describedby', () => {
+    test('accepts both error id and underline id', () => {
       const tree = renderer.create(
         <TextInput
-          errorMessage="Uh oh"
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('errorProps populate the error tag', () => {
-      const tree = renderer.create(
-        <TextInput
-          errorMessage="Uh oh"
           errorProps={{
-            id: 'error-tag',
-            className: 'more classes',
-            'data-message': 'hi there'
+            id: 'error-id'
           }}
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-  });
-
-  describe('Underline', () => {
-    test('underline acts as child prop', () => {
-      const tree = renderer.create(
-        <TextInput
-          underline="More explanation"
-          />
-      ).toJSON();
-
-      expect(tree).toMatchSnapshot();
-    });
-
-    test('underlineProps populate the underline tag', () => {
-      const tree = renderer.create(
-        <TextInput
-          underline="Fill this in because ..."
           underlineProps={{
-            className: 'help explain',
-            style: { background: 'blue' }
+            id: 'underline-id'
           }}
           />
       ).toJSON();
 
       expect(tree).toMatchSnapshot();
-    });
-
-    describe('id', () => {
-      test('defaults to underlineProps.id', () => {
-        const tree = renderer.create(
-          <TextInput
-            underline="More info"
-            underlineProps={{
-              id: "descriptor"
-            }}
-            ariaDescribedby="secondary"
-            />
-        ).toJSON();
-
-        expect(tree).toMatchSnapshot();
-      });
-
-      test('will accept ariaDescribedby prop as a fallback', () => {
-        const tree = renderer.create(
-          <TextInput
-            underline="Extra info"
-            ariaDescribedby="secondary"
-            />
-        ).toJSON();
-
-        expect(tree).toMatchSnapshot();
-      });
-    });
-  });
+    })
+  })
 });
