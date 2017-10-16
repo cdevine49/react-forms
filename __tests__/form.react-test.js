1b8be5f3d6b507f8b36b1b1f6317e1fbff3551a8
Conor Devine <conorjdevine@gmail.com>
2017-10-15 22:06:27 -0400

    change syntax

--- __tests__/form.react-test.js
+++ __tests__/form.react-test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import FormContainer from '../lib/components/form';
 import TextField from '../lib/components/textField';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import 'jest-styled-components';
+
+test('default state', () => {
+  const form = shallow(
+    <FormContainer />
+  );
+
+  expect(form.state().displayErrors).toBe(false);
+  expect(form.state().submittable).toEqual([])
+});
 
 describe("Submission", () => {
   let onSubmit;
@@ -19,27 +28,27 @@ describe("Submission", () => {
 
 
     form.setState({ submittable: [] });
-    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit).not.toHaveBeenCalled();
     form.simulate('submit');
-    expect(onSubmit.mock.calls.length).toBe(1);
+    expect(onSubmit).toHaveBeenCalledTimes(1);
   });
 
   test("calls onSubmit prop when submittable array elements all truthy", () => {
     form.setState({ submittable: [ true, 'true', 1] });
-    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit).not.toHaveBeenCalled();
     form.simulate('submit');
-    expect(onSubmit.mock.calls.length).toBe(1);
+    expect(onSubmit).toHaveBeenCalledTimes(1);
   });
 
   test("does not call onSubmit prop when any submittable array elements falsey", () => {
-    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit).not.toHaveBeenCalled();
     form.setState({ submittable: [ true, 'true', false, 1] });
     form.simulate('submit');
 
-    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit).not.toHaveBeenCalled();
 
     form.setState({ submittable: [ true, 'true', '', 1] });
     form.simulate('submit');
-    expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit).not.toHaveBeenCalled();
   });
 });
