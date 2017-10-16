67cf40f029c59b55af06c1647f194161ea364f98
Conor Devine <conorjdevine@gmail.com>
2017-10-15 20:20:14 -0400

    submit button unnecessary for testing

--- __tests__/form.react-test.js
+++ __tests__/form.react-test.js
@@ -1,50 +1,45 @@
 import React from 'react';
-import Form from '../lib/components/form';
+import FormContainer from '../lib/components/form';
 import TextField from '../lib/components/textField';
-import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import 'jest-styled-components';
 
-describe("Form", () => {
-  describe("Submission", () => {
-    let onSubmit;
-    const form = mount(
-      <Form>
-        <input type="submit" />
-      </Form>
-    );
+describe("Submission", () => {
+  let onSubmit;
+  const form = mount(
+    <FormContainer />
+  );
 
-    beforeEach(() => {
-      onSubmit = jest.fn();
-      form.setProps({ onSubmit });
-    });
+  beforeEach(() => {
+    onSubmit = jest.fn();
+    form.setProps({ onSubmit });
+  });
 
-    test("calls onSubmit prop when submittable array empty", () => {
+  test("calls onSubmit prop when submittable array empty", () => {
 
 
-      form.setState({ submittable: [] });
-      expect(onSubmit.mock.calls.length).toBe(0);
-      form.simulate('submit');
-      expect(onSubmit.mock.calls.length).toBe(1);
-    });
+    form.setState({ submittable: [] });
+    expect(onSubmit.mock.calls.length).toBe(0);
+    form.simulate('submit');
+    expect(onSubmit.mock.calls.length).toBe(1);
+  });
 
-    test("calls onSubmit prop when submittable array elements all truthy", () => {
-      form.setState({ submittable: [ true, 'true', 1] });
-      expect(onSubmit.mock.calls.length).toBe(0);
-      form.simulate('submit');
-      expect(onSubmit.mock.calls.length).toBe(1);
-    });
+  test("calls onSubmit prop when submittable array elements all truthy", () => {
+    form.setState({ submittable: [ true, 'true', 1] });
+    expect(onSubmit.mock.calls.length).toBe(0);
+    form.simulate('submit');
+    expect(onSubmit.mock.calls.length).toBe(1);
+  });
 
-    test("does not call onSubmit prop when any submittable array elements falsey", () => {
-      expect(onSubmit.mock.calls.length).toBe(0);
-      form.setState({ submittable: [ true, 'true', false, 1] });
-      form.simulate('submit');
+  test("does not call onSubmit prop when any submittable array elements falsey", () => {
+    expect(onSubmit.mock.calls.length).toBe(0);
+    form.setState({ submittable: [ true, 'true', false, 1] });
+    form.simulate('submit');
 
-      expect(onSubmit.mock.calls.length).toBe(0);
+    expect(onSubmit.mock.calls.length).toBe(0);
 
-      form.setState({ submittable: [ true, 'true', '', 1] });
-      form.simulate('submit');
-      expect(onSubmit.mock.calls.length).toBe(0);
-    });
+    form.setState({ submittable: [ true, 'true', '', 1] });
+    form.simulate('submit');
+    expect(onSubmit.mock.calls.length).toBe(0);
   });
 });
